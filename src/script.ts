20d617e9f0485da25afecef2e7d61bf22c3d6646
Jerzy Rutkowski <jerzyx11@gmail.com>
2022-06-12 17:59:43 +0200

    EL TUMERO CAME WITH CLUTCH (SAVED)

--- src/script.ts
+++ src/script.ts
@@ -1,79 +1,51 @@
-// import gui from "./gui";
-
+// document.querySelector('canvas')!.style.cursor = "none";
 const canvas = document.getElementById('canvas') as HTMLCanvasElement | null;
-const canvasDiv: HTMLElement | null = document.querySelector("canvas")
 const ctx:CanvasRenderingContext2D = canvas?.getContext("2d") as CanvasRenderingContext2D;
 canvas!.height = 960
 canvas!.width = 960
+
 
 let selectedBrush:number = 1
 let mouseDown:boolean = false;
 
 document.body.onmousedown = function(event) {
-    let mousePosition = getMousePos(event);   
+    let mousePosition = getMousePos(event);
     mouseDown = true;
-    console.log(mouseDown)
-
     canvas?.addEventListener('mousemove', (event) => {
         if(mouseDown == true){
+            if(selectedBrush == 0){
+                ctx.fillStyle = "#34495e";
+            } 
+            if (selectedBrush == 1){
+                ctx.fillStyle = "#FFFFFF";
+            }
             let mousePosition = getMousePos(event); 
-            console.log(mousePosition)
-            ctx.fillStyle = "rgba(255,255,255)"
-            ctx?.fillRect(mousePosition.x * 32, mousePosition.y * 32,32,32)
+            // console.log(mousePosition)
+            ctx?.fillRect((mousePosition.x * 32),(mousePosition.y * 32),32,32)
         }
     })
 }
 
-
 document.body.onmouseup = function() {
   mouseDown = false;
-  console.log(mouseDown)
 }
-
-// document.querySelector('canvas')!.style.cursor = "none";
-
-
-// checkerboard
-function drawTiles(ctx:CanvasRenderingContext2D){
-    ctx.fillStyle = "#34495e";
-
-    const nRow:number = 32;    
-    const nCol:number = 32;
-
-    const width:number = 32;
-    const height:number = 32;
-
-    for (var i = 0; i < nRow; ++i) {
-        for (var j = 0, col = nCol / 2; j < col; ++j) {
-            ctx.rect(2 * j * width + (i % 2 ? 0 : width), i * height, width, height);
-        }
-    }
-    ctx.fill();
-}
-drawTiles(ctx)
-
 
 // mousepos + cursor 
 function getMousePos(event:MouseEvent) {
     var rect = canvas?.getBoundingClientRect();
     return {
-      x: Math.round((event.clientX - rect!.left) / 32.5 ) ,
-      y: Math.round((event.clientY - rect!.top) / 32.5)
+      x: Math.round((event.clientX - rect!.left) / 32) ,
+      y: Math.round((event.clientY - rect!.top) / 32)
     };
-    
-    
 }
 
 canvas?.addEventListener("mousemove", (event:MouseEvent) => {
     let mousePosition = getMousePos(event);
-    // console.log(getMousePos(event));
-    // ctx.strokeRect(mousePosition.x*32,mousePosition.y*32,32,32)
   });
 
   canvas?.addEventListener("click", (event:MouseEvent) => {
     let mousePosition = getMousePos(event);
     if(selectedBrush == 0) {
-        // console.log("erasing")
         ctx.fillStyle = "#34495e";
         ctx?.fillRect(mousePosition.x * 32, mousePosition.y * 32,32,32)
     }
@@ -82,3 +54,13 @@ canvas?.addEventListener("mousemove", (event:MouseEvent) => {
         ctx?.fillRect(mousePosition.x * 32, mousePosition.y * 32,32,32)
     }    
   });
+
+  document.getElementById("gui-brush")?.addEventListener("click", (e:Event) => {
+    selectedBrush = 1
+    console.log("brush selected")
+})
+
+document.getElementById("gui-eraser")?.addEventListener("click", (e:Event) => {
+    selectedBrush = 0
+    console.log("eraser selected")
+})
